4cb252f4bb98ffd34cc18c7ab45e65615097f1a3
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-12-13 22:42:57 +0330

    Update client to read input continuously

--- client.py
+++ client.py
@@ -4,17 +4,24 @@ SERVER_HOST = "localhost"
 SERVER_PORT = 8081
 
 def main():
+    # We set socket type to socket.SOCK_STREAM which means that it will use TCP
     cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cs.connect((SERVER_HOST, SERVER_PORT))
-    cs.send(b"GET user2")
 
-    res = cs.recv(1024).decode()
+    while True:
+        in_str = input("Please input your command or EXIT to exit: ")
 
-    print(res)
+        cs.send(bytes(in_str, encoding='utf-8'))
+        res = cs.recv(1024).decode()
+        print("Response received:")
+        print(res)
 
+        if in_str == "EXIT":
+            break
+
+        
     cs.close()
     
-
 
 if __name__ == '__main__':
     main()
