c8a42e5655c25106320a9b47e1d68ddae75aae41
Asal Khaef <asal.khaef@gmail.com>
2023-12-16 01:40:33 +0330

    move some parts to method

--- nmap.py
+++ nmap.py
@@ -1,14 +1,18 @@
 import socket
 
-def main():
-    host = input("Enter host: ")
-    min_port = int(input("Enter start range: "))
-    max_port = int(input("Enter end range: "))
+def is_port_open(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host,port))
+        return True
+    except:
+        return False
+    finally:
+        s.close()
 
-    for port in range(min_port,max_port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((host,port))
+def check_port_range(host, s_port, e_port):
+    for port in range(s_port, e_port):
+        if is_port_open(host, port):
             print(f"Port {port} is open.")
             try:
                 serviceName = socket.getservbyport(port, "tcp")
@@ -18,14 +22,17 @@ def main():
                 print(f"UDP service: {serviceName}")
             except Exception as error:
                 print(f"service port:{error}")
-
-        except Exception as error:
+        else:
             print(f"Port {port} is close.")
-            print(error)
+
+
+def main():
+    host = input("Enter host: ")
+    min_port = int(input("Enter start range: "))
+    max_port = int(input("Enter end range: "))
+    #check...
+    check_port_range(host, min_port, max_port)
         
-        finally:
-            s.close()
-
-
+        
 if __name__ == '__main__':
     main()
