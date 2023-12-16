bca404840d9edc78f1dcf270f00af9d8748a3d96
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-12-16 17:58:25 +0330

    Add host availability function

--- nmap.py
+++ nmap.py
@@ -2,6 +2,7 @@ import socket
 
 def is_port_open(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5) # Setting timeout to 5 seconds
     try:
         s.connect((host,port))
         return True
@@ -18,20 +19,36 @@ def check_port_range(host, s_port, e_port):
                 serviceName = socket.getservbyport(port, "tcp")
                 print(f"TCP service: {serviceName}")
                 serviceName = socket.getservbyport(port, "udp")
-                print(serviceName)
                 print(f"UDP service: {serviceName}")
             except Exception as error:
                 print(f"service port:{error}")
         else:
             print(f"Port {port} is close.")
 
+def is_host_online(host):
+    PORTS = [20, 21, 22, 25, 53, 80, 123, 443]
+    is_online = False
+    for port in PORTS:
+        if is_port_open(host, port):
+            print(f"Port {port} is open, so host is online!")
+            is_online = True
+            break
+
+    if not is_online:
+        print("Specified ports were all close, so host is not online!")
 
 def main():
-    host = input("Enter host: ")
-    min_port = int(input("Enter start range: "))
-    max_port = int(input("Enter end range: "))
-    #check...
-    check_port_range(host, min_port, max_port)
+    mode = input("Enter 1 to check host availability or 2 to check a range of ports: ")
+    if mode == "1":
+        host = input("Enter host: ")
+        is_host_online(host)
+    elif mode == "2":
+        host = input("Enter host: ")
+        min_port = int(input("Enter start range: "))
+        max_port = int(input("Enter end range: "))
+        check_port_range(host, min_port, max_port)
+    else:
+        print("Input was not valid.")
         
         
 if __name__ == '__main__':
